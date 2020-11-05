84a272ca8805cbe45780021d7925baf64159905a
Arx1971 <adnan.rahin13@gmail.com>
2020-11-05 16:48:13 -0500

    Update

--- src/main/java/com/astronaut/space/jdbc/application/AstronautChildRetrieve.java
+++ src/main/java/com/astronaut/space/jdbc/application/AstronautChildRetrieve.java
@@ -1,11 +1,14 @@
 package com.astronaut.space.jdbc.application;
 
 import com.astronaut.space.jdbc.dao.connection.manager.Gateway;
+import com.astronaut.space.jdbc.model.Astronaut;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AstronautChildRetrieve {
 
@@ -14,15 +17,27 @@ public class AstronautChildRetrieve {
         try {
             Connection connection = Gateway.getDBConnection();
             Statement statement = connection.createStatement();
-            String query = "select * from astronaut_child_info";
+            String query = "select * from astronaut_info";
 
             ResultSet rs = statement.executeQuery(query);
 
-            System.out.println(rs);
+            List<Astronaut> astronauts = new ArrayList<>();
 
             while (rs.next()) {
-                System.out.println(rs.getString("astronaut_child_name"));
+                int id = Integer.parseInt(rs.getString("astronaut_id"));
+                String fName = rs.getString("astronaut_fname");
+                String lName = rs.getString("astronaut_lname");
+                String dob = rs.getString("astronaut_fname");
+                String country = rs.getString("astronaut_country");
+                String gender = rs.getString("astronaut_gender");
+                Astronaut astronaut = new Astronaut.Builder(id, fName, lName).country(country).build();
+                astronauts.add(astronaut);
             }
+
+            for(Astronaut astronaut: astronauts){
+                System.out.println(astronaut);
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
