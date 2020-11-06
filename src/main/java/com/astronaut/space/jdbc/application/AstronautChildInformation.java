58de9cc67b3590e7d1ae4223ef7f8cb8b1e5f970
Arx1971 <adnan.rahin13@gmail.com>
2020-11-05 22:57:29 -0500

    test

--- src/main/java/com/astronaut/space/jdbc/application/AstronautChildInformation.java
+++ src/main/java/com/astronaut/space/jdbc/application/AstronautChildInformation.java
@@ -1,10 +1,36 @@
 package com.astronaut.space.jdbc.application;
 
+import com.astronaut.space.jdbc.dao.connection.manager.Gateway;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class AstronautChildInformation {
 
     public static void main(String[] args) {
 
-        
+        try {
+            Connection connection = Gateway.getDBConnection();
+            Statement statement = connection.createStatement();
+            String sql = "select astronaut_child_name\n" +
+                    "from astronaut_info, astronaut_child_info\n" +
+                    "where astronaut_info.astronaut_id = 9 and\n" +
+                    "astronaut_info.astronaut_id = astronaut_child_info.astronaut_id;";
+
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("astronaut_child_name"));
+            }
+
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
 
     }
 
