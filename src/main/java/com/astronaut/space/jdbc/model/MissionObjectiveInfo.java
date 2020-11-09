dbd36d20465398befca143b5459f7a994958b85b
Arx1971 <adnan.rahin13@gmail.com>
2020-11-08 22:17:52 -0500

    Update

--- src/main/java/com/astronaut/space/jdbc/model/MissionObjectiveInfo.java
+++ src/main/java/com/astronaut/space/jdbc/model/MissionObjectiveInfo.java
@@ -2,6 +2,30 @@ package com.astronaut.space.jdbc.model;
 
 public class MissionObjectiveInfo {
 
+    private int id;
+    private String missionObjectiveName;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMissionObjectiveName() {
+        return missionObjectiveName;
+    }
+
+    public void setMissionObjectiveName(String missionObjectiveName) {
+        this.missionObjectiveName = missionObjectiveName;
+    }
+
+    @Override
+    public String toString() {
+        return "MissionObjectiveInfo{" +
+                "id=" + id +
+                ", missionObjectiveName='" + missionObjectiveName + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/astronaut/space/jdbc/model/SpaceShipInfo.java
+++ src/main/java/com/astronaut/space/jdbc/model/SpaceShipInfo.java
@@ -2,6 +2,30 @@ package com.astronaut.space.jdbc.model;
 
 public class SpaceShipInfo {
 
+    private int id;
+    private String spaceShipName;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSpaceShipName() {
+        return spaceShipName;
+    }
+
+    public void setSpaceShipName(String spaceShipName) {
+        this.spaceShipName = spaceShipName;
+    }
+
+    @Override
+    public String toString() {
+        return "SpaceShipInfo{" +
+                "id=" + id +
+                ", spaceShipName='" + spaceShipName + '\'' +
+                '}';
+    }
 }
