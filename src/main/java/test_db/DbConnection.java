ef12eda5520c7cf5b7301a1ac586b8b87bcc1454
Arx1971 <adnan.rahin13@gmail.com>
2020-11-12 23:20:20 -0500

    Change to data base connection

--- src/main/java/test_db/DbConnection.java
+++ src/main/java/test_db/DbConnection.java
@@ -3,6 +3,10 @@ package test_db;
 import com.astronaut.space.jdbc.model.MissionLandingSite;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DbConnection {
 
@@ -12,29 +16,41 @@ public class DbConnection {
         String user = "root";
         String password = "root";
 
+        List<Map<String, Object>> resultList = new ArrayList<>();
+        Map<String, Object> row = new HashMap<>();
+
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
 
             Statement statement = connection.createStatement();
 
-            String sql = "select * from mission_landing_site";
+            String sql = "select astronaut_info.astronaut_id,\n" +
+                    "astronaut_info.astronaut_fname,\n" +
+                    "astronaut_info.astronaut_lname,\n" +
+                    "university_info.university_name,\n" +
+                    "degree_info.degree_name\n" +
+                    "from astronaut_info,\n" +
+                    "university_info,\n" +
+                    "degree_info,\n" +
+                    "astronaut_education_info\n" +
+                    "where astronaut_info.astronaut_id = astronaut_education_info.astronaut_id\n" +
+                    "and degree_info.degree_id = astronaut_education_info.degree_id\n" +
+                    "and university_info.university_id = astronaut_education_info.university_id;\n";
 
             ResultSet rs = statement.executeQuery(sql);
 
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            Integer columnCount = metaData.getColumnCount();
+
             while (rs.next()) {
-                int id = Integer.parseInt(rs.getString("mission_landing_site_id"));
-                String landingSiteName = rs.getString("mission_landing_site_name");
-                String landingSiteCity = rs.getString("mission_landing_site_city");
-                String landingSiteState = rs.getString("mission_landing_site_state");
-                String landingSiteCountry = rs.getString("mission_landing_site_country");
-
-                MissionLandingSite missionLandingSite = new MissionLandingSite.Builder(id).
-                        landingSiteName(landingSiteName).ladingSiteCity(landingSiteCity)
-                        .landingSiteState(landingSiteState).landingSiteCountry(landingSiteCountry).build();
-
-                System.out.println(missionLandingSite);
-
+                for (int i = 1; i <= columnCount; i++) {
+                    row.put(metaData.getColumnName(i), rs.getObject(i));
+                }
+                resultList.add(row);
             }
+
+            System.out.println(resultList);
 
         } catch (SQLException e) {
             e.printStackTrace();
