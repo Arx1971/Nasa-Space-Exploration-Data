68aedb2d4e6781ae7a4c215a1c174ddb54d0744a
Arx1971 <adnan.rahin13@gmail.com>
2020-11-15 21:45:58 -0500

    User API

---
+++ src/main/java/com/astronaut/space/hibernate/service/serviceInterface/APIInterface.java
@@ -0,0 +1,11 @@
+package com.astronaut.space.hibernate.service.serviceInterface;
+
+import com.astronaut.space.hibernate.entity.AstronautInfo;
+
+import java.util.List;
+
+public interface APIInterface {
+
+    List<AstronautInfo> getAstronautInformation();
+
+}
