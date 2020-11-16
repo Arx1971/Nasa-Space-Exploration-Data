b19e36ac3bd27b0535871646b6853d9546a87484
Arx1971 <adnan.rahin13@gmail.com>
2020-11-15 19:52:51 -0500

    Application Server Tested

---
+++ src/main/java/com/astronaut/space/hibernate/application/Application.java
@@ -0,0 +1,9 @@
+package com.astronaut.space.hibernate.application;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/astronaut/space/hibernate/entity/AstronautInfo.java
@@ -0,0 +1,84 @@
+package com.astronaut.space.hibernate.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "astronaut_info")
+public class AstronautInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "astronaut_id")
+    private Integer id;
+    @Column(name = "astronaut_fname")
+    private String fName;
+    @Column(name = "astronaut_lname")
+    private String lName;
+    @Column(name = "astronaut_dob")
+    private String dob;
+    @Column(name = "astronaut_country")
+    private String country;
+    @Column(name = "astronaut_Gender")
+    private String astronautGender;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAstronautGender() {
+        return astronautGender;
+    }
+
+    public void setAstronautGender(String astronautGender) {
+        this.astronautGender = astronautGender;
+    }
+
+    @Override
+    public String toString() {
+        return "AstronautInfo{" +
+                "id=" + id +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", dob='" + dob + '\'' +
+                ", country='" + country + '\'' +
+                ", astronautGender='" + astronautGender + '\'' +
+                '}';
+    }
+}
